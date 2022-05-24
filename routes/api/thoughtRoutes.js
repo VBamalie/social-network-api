c01a82986dd890a26776642582d08ed4df812908
Amalie Von Behren <vbamalie@gmail.com>
2022-05-24 15:55:23 -0400

    routes made

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,24 @@
 //import the needed modules
-
+const router = require("express").Router();
+const{
+    getThoughts,
+    getSingleThought,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    removeReaction
+} = require("../../controllers/thoughtController")
 //api/thoughts route
-
+router.route("/").get(getThoughts).post(createThoughts);
 //api.thoughts: id
+router.route("/:thoughtsId").get(getSingleThought).put(updateThoughts).delete(deleteThoughts);
 
 //api.thoughts/:id/ reactions route
+router.route("/:thoughtsId/reactions").post(addReaction)
 
 //api/thoughts/:thoughts id/ reactions/ id
+router.route("/:thoughtsId/reactions/:reactionId").delete(removeReaction)
 
 //export
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -14,6 +14,10 @@ router.route("/").get(getUsers).post(createUser);
 
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
+router.route("/:userId/friends").post(addFriend)
+
+router.route("/:userId/friends/:friendId").delete(removeFriend)
+
 //api/user.userid/frinds/friendid route
 
 module.exports = router;
